7ffb5b858f5ffdb9f170e62f91465a8a01d125ba
Phillip-M-Ebdon <54140329+Phillip-M-Ebdon@users.noreply.github.com>
2022-05-13 16:26:23 +1000

    Update streamlit_app.py
    ordering and multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,21 @@
 import streamlit
 import pandas 
 
-streamlit.title('My Parenets New Healthy Diet')
+# data stuff
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
+# streamlit stuff
+streamlit.title('My Parenets New Healthy Diet')
+## breakfast
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+## smoothies
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
-my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+# smoothie ingredient table
 streamlit.dataframe(my_fruit_list)
