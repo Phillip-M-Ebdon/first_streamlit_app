6e8fbbedfba928832788deae1c197f270273a34c
Phillip-M-Ebdon <54140329+Phillip-M-Ebdon@users.noreply.github.com>
2022-05-24 20:48:22 +1000

    typo augh

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,5 @@ import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response.json())
 
-fruit_normalized = pandas.json_normalize(fruitvice_response.json())
+fruit_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlist.dataframe(fruit_normalized)
