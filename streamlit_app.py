844ef994b3bf06365dcbd1a28abc1600e7950374
Phillip-M-Ebdon <54140329+Phillip-M-Ebdon@users.noreply.github.com>
2022-05-24 20:55:43 +1000

    user input for api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas 
+import requests
+
 
 # data stuff
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
@@ -22,10 +24,14 @@ fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+# API response area
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + 'kiwi')
+# get user input
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
+# make web request
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 fruit_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruit_normalized)
